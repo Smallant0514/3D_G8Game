23692d29f263fb35d6f45a9de0b843031c4cfcbf
Smallant0514 <55477183+Smallant0514@users.noreply.github.com>
2019-11-20 15:55:00 +0800

    自定義參數

--- Unity Project/Assets/Scripts/Chicken.cs
+++ Unity Project/Assets/Scripts/Chicken.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine;
+﻿using UnityEngine; //引用unity API
 
+//類別 類別名稱
 public class Chicken : MonoBehaviour
 {
-    int speed = 10;
-    float turn = 2.5f;
-    bool mission = false;
-    string Name = "G8雞";
+    // 宣告變數 (定義欄位 Field)
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
+    // 私人 - 隱藏 private (預設)
+    // 公開 - 顯示 public
+
+    [Header("移動速度")][Range(1,100)] // Range 限制欄位範圍
+    public int speed = 10;
+    [Header("旋轉速度"),Tooltip("G8雞的旋轉速度"),Range(1.5f,200f)] // Tooltip 用在提示較長資訊
+    public float turn = 2.5f;
+    [Header("是否完成任務")]
+    public bool mission = false;
+    [Header("玩家名稱")]
+    public string Name = "G8雞";
+
+    public void Run(int speed)
+    {
+
+    }
         
 }
---
+++ Unity Project/Assets/Scripts/LearnMethod.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    // 定義方法
+    // 語法:
+    // 修飾詞 傳回類型 方法名稱 (參數) { 陳述式或演算法 }
+    // 參數類型 參數名稱
+    // void 無傳回
+    // 自定義方法需要被呼叫
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="speed">開車的速度</param>
+    private void Drive(int speed)
+    {
+        // 輸出訊息(字串)
+        print("音效~");
+        // 可使用 + 號串接字串與其他類型
+        print("開車,時速:" + speed);
+    }
+
+    // 有預設值的參數 選填式參數，呼叫時可不填寫
+    // 有選填式參數只能寫在最右邊
+    public void Shoot(int count,float speed,string prop = "無",string direction = "前方")
+    {
+        print("弓箭數量:" + count);
+        print("弓箭速度:" + speed);
+        print("弓箭屬性:" + prop);
+        print("弓箭方向:" + direction);
+
+    }
+
+    // 事件 : 在指定的時間會以指定次數執行的方法
+    // 初始事件 : 遊戲播放後執行一次
+    private void Start()
+    {
+        print("哈囉，沃德~");
+
+        Drive(200); // 呼叫自訂方法 寫幾次就叫幾次 需給予對應的引數
+        Drive(300); // 裡面數字傳回到上面寫的speed 
+
+        Shoot(1, 1.5f);
+        Shoot(10,10.5f,"火屬性");
+        Shoot(3, 2, direction: "前後方"); // 用指名的方式設定參數
+    }
+}
